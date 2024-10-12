72c6ed41c6a2c5312736cdd3c1b189cd6654f5ba
dextermadh <madhukaabhishek30@gmail.com>
2024-10-12 06:56:52 +0530

    feat: initial commit

---
+++ backend/src/routes/tasksRoutes.js
@@ -0,0 +1,14 @@
+import express from "express";
+import { createTask, deleteTask, getTask, getTasks, updateTask } from "../controllers/auth/task/taskController.js";
+import { protect } from "../middleware/authMiddleware.js";
+
+const router = express.Router();
+
+
+router.post("/task/create", protect, createTask)
+router.get("/tasks", protect, getTasks)
+router.get("/task/:id", protect, getTask)
+router.patch("/task/:id", protect, updateTask)
+router.delete("/task/:id", protect, deleteTask)
+
+export default router;
